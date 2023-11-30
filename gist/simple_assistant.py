87ea61b1440d8e73e94f7aef52cf7a39af262b6c
Sourcery AI <>
2023-11-30 21:41:29 +0000

    'Refactored by Sourcery'

--- gist/simple_assistant.py
+++ gist/simple_assistant.py
@@ -81,8 +81,7 @@ class AssistantManager:
 # Assume 'get_stock_price' function is defined elsewhere
 def get_stock_price(symbol: str) -> float:
     stock = yf.Ticker(symbol)
-    price = stock.history(period="1d")['Close'].iloc[-1]
-    return price
+    return stock.history(period="1d")['Close'].iloc[-1]
 
 # JSON schema and function mapping
 tools_list = [{
